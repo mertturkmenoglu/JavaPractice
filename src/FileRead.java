90ba3d9514725e2fded39de4871d8e02d9e00381
Mert <mertturkmenoglu99@gmail.com>
2019-10-04 23:05:47 +0300

    Update FileRead class

--- src/FileRead.java
+++ src/FileRead.java
@@ -1,24 +1,28 @@
-
 import java.io.File;
 import java.text.SimpleDateFormat;
 
 public class FileRead implements Runnable {
     @Override
-    synchronized public void run() {
+    public void run() {
         while (true) {
             File file = new File("c:\\Users\\Cagdas\\Desktop\\ss\\ss.png");
             System.out.println(file);
-            SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
-            String date = sdf.format(file.lastModified());
+
+            SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+            String date = formatter.format(file.lastModified());
             System.out.println(date);
-            String[] dates = date.split( " ");
+
+            String[] dates = date.split("\\s+");
+
             for (String temp : dates) {
                 System.out.println(temp);
             }
+
             try {
                 Thread.sleep(6000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+                break;
             }
         }
     }
